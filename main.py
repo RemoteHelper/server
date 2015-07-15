82b68a27addc0cc986a0007956dcb4d1a1d11833
Ángel Sanz <angelsanzgit@gmail.com>
2015-07-15 15:21:38 +0200

    Implement basic static page generation and routing.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 import json
-from bottle import route, run, post, request, HTTPResponse
+import uuid
+
+from bottle import route, run, abort, post, get, request, HTTPResponse, template
 
 MEDIA = None
 MEDIA_TYPE = None
 EVENTS_URL = None
+
+STATIC_PAGES = {}
 
 # -> {}
 def load_config(config_file):
@@ -18,18 +22,38 @@ def process_help():
     MEDIA = request.json['media']['content']
     MEDIA_TYPE = request.json['media']['type']
     EVENTS_URL = request.json['eventsURL']
-    return {"userURL": user_url, "doneURL": done_url}
+
+    generate_static_page()
+
+    return {"userURL": USER_URL, "doneURL": DONE_URL}
+
+def generate_static_page():
+    if MEDIA_TYPE == 'url':
+        static_page_html = template('static_user_page_by_url', media_url=MEDIA)
+        a_new_uuid = generate_page_uuid()
+        STATIC_PAGES[a_new_uuid] = static_page_html
+        USER_URL = host + config['user_endpoint'] + a_new_uuid
+
+def generate_page_uuid():
+    return str(uuid.uuid4())
+
+@get('/resolve/<an_uuid>')
+def serve_static_page(an_uuid):
+    if an_uuid in STATIC_PAGES:
+        return STATIC_PAGES[an_uuid]
+
+    abort(404, 'Not found')
+
 
 # {} -> Response[status: Either<200|403> body?]
 @post('/api/done')
 def stop_help():
     return HTTPResponse(status=200) \
-        if request.json and request.json['authURL'] == user_url \
+        if request.json and request.json['authURL'] == USER_URL \
         else HTTPResponse(status=403, body="Auth url doesn't match!")
 
 if __name__ == "__main__":
     config = load_config('config.json')
     host = config['host']['protocol'] + config['host']['url'] + ":" + str(config['host']['port'])
-    user_url = host + config['user_endpoint']
-    done_url = host + config['done_endpoint']
+    DONE_URL = host + config['done_endpoint']
     run(host=config['host']['url'], port=config['host']['port'], debug=True)

fd5577f993c8e1e6444cf3f1e60aa01430147a5e
Ángel Sanz <angelsanzgit@gmail.com>
2015-08-10 19:37:30 +0200

    Implement a JobContainer.
    This allows us to get rid of the global variable
    and encapsulates serial job creation, access and
    testing for existence.

--- server/job.py
+++ server/job.py
@@ -1,17 +1,6 @@
-#!/usr/bin/env python3
-
-
 class Job:
     def __init__(self, events_url):
-        self._completed = False
         self._events_url = events_url
-
-    def complete_job(self):
-        self._events_url = None
-        self._completed = True
-
-    def is_complete(self):
-        return self._completed
 
     def get_events_url(self):
         return self._events_url

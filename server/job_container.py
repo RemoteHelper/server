4d73e75186feb422ac9eaf44bcdd2184d80c1489
Ángel Sanz <angelsanzgit@gmail.com>
2015-08-18 13:40:28 +0200

    Implement detection of 'doneText' payload in 'done' response.
    See https://github.com/RemoteHelper/helper-API-spec/commit/4b561f0857d9024c2fb9d43beccbfdad23bc6041.
    
    This implements all logic needed to receive the doneText
    and show it in the static page.

--- server/job_container.py
+++ server/job_container.py
@@ -1,22 +1,15 @@
-import job
+from server.job import Job, NullJob
 
 class JobContainer:
     """
-    A container for a single Job instance. It can be in either of two states,
-    'complete' or 'running'.
+    A container for a single Job instance.
     """
 
-    def create_new(self, events_url):
-        self._job = job.Job(events_url)
+    def __init__(self):
+        self._job = NullJob()
 
-    def is_running(self):
-        return hasattr(self, '_job')
+    def create_new_job(self, events_url):
+        self._job = Job(events_url)
 
-    def is_complete(self):
-        return not self.is_running()
-
-    def complete(self):
-        del self._job
-
-    def get_events_url(self):
-        return self._job.get_events_url()
+    def get(self):
+        return self._job
---
+++ test/job_container_test.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+from server.job_container import JobContainer
+from server.job import NullJob, Job
+
+class JobContainerTest(TestCase):
+    def setUp(self):
+        self.a_job_container = JobContainer()
+
+    def test_is_initialised_with_a_null_job(self):
+        self.assertIs(type(self.a_job_container._job), NullJob)
+
+    def test_creates_a_job_instance_when_create_new_job_is_called(self):
+        self.a_job_container.create_new_job('some_url')
+
+        self.assertIs(type(self.a_job_container._job), Job)
+
+    def test_throws_away_old_instance_when_create_new_job_is_called(self):
+        old_job = self.a_job_container._job
+
+        self.a_job_container.create_new_job('some_url')
+
+        self.assertIsNot(self.a_job_container._job, old_job)
+
+    def test_exposes_the_currently_conatined_job(self):
+        self.assertIs(self.a_job_container.get(), self.a_job_container._job)
+
+
+if __name__ == '__main__':
+    unittest.main()
